8e9ee9da0f796796cfb43e0d40d815583e006db4
Guksu <yung3152@daum.net>
2021-12-06 11:56:02 +0900

    로그아웃

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { useHistory } from "react-router";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { client } from "../apollo";
 import { isLogginAtom } from "../atom";
 
 function Header() {
   const isLoggin = useRecoilValue(isLogginAtom);
+  const setIsLogin = useSetRecoilState(isLogginAtom);
+
   const history = useHistory();
+  const logOutClick = () => {
+    localStorage.clear();
+    client.clearStore();
+    setIsLogin(false);
+  };
   return (
     <>
       <h1 onClick={() => history.push("/")}>Intengible Market</h1>
       <span onClick={() => history.push("/product")}>상품</span>
       <span onClick={() => history.push("/board")}>게시판</span>
       {isLoggin ? (
-        <span onClick={() => history.push("/profile")}>프로필</span>
+        <>
+          <span onClick={() => history.push("/profile")}>프로필</span>
+          <span onClick={logOutClick}>로그아웃</span>
+        </>
       ) : (
         <span onClick={() => history.push("/login")}>로그인</span>
       )}
